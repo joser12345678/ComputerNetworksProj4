0c8858cdb0b27223be9e86231b32627c6d3a2222
joser12345678 <joser17@vt.edu>
2022-11-07 23:28:09 +0000

    first commit

---
+++ tcp_traceroute.py
@@ -0,0 +1,36 @@
+import argparse
+import socket
+import scapy
+
+MAX_HOPS = 30
+DST_PORT = 80
+TARGET = "NOT SPECIFIED"
+
+def setParams():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', metavar='MAX_HOPS', type=int, help="Max hops to probe (default = 30)")
+    parser.add_argument('-p', metavar='DST_PORT', type=str, help="TCP destination port (default = 80)")
+    parser.add_argument('-t', metavar='TARGET', type=str,
+                        help="Target domain or IP")
+
+
+    # parse arguments and put into args
+    args = parser.parse_args()
+    global TARGET, MAX_HOPS, DST_PORT
+
+    # a target must always be specified
+    if not args.t:
+        print("No target specified. Terminating.")
+        exit()
+    else:
+        TARGET = args.t
+    
+    # set optional arguments
+    if args.m:
+        MAX_HOPS = args.m
+    if args.p:
+        DST_PORT = args.p
+
+
+if __name__ == '__main__':
+    setParams()
