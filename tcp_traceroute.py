765c4e1e43edac6a18336bc41770dae7aed3e8f9
joser12345678 <joser17@vt.edu>
2022-11-08 22:21:02 +0000

    functionalized sending and recieving probes. Also figured out the id and ttl will be used to distinguish probes of the same type

--- tcp_traceroute.py
+++ tcp_traceroute.py
@@ -1,12 +1,35 @@
 import argparse
 import socket
 from sys import flags
-from scapy.all import IP, TCP, Ether
+from scapy.all import IP, TCP, Ether, raw
+import time
 
 MAX_HOPS = 30
 DST_PORT = 80
 TARGET = ""
-HOST = "172.17.149.28"
+
+
+
+# sends a single probe with specified ttl
+def sendProbe(my_ttl, my_id):
+    ip = IP(dst=TARGET, ttl=my_ttl, id = my_id)
+    tcp = TCP(sport=recv_sock.getsockname()[1], dport=DST_PORT, flags="S")
+    packet = ip/tcp
+    print(IP(raw(packet))[IP].id)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    s.sendto(bytes(packet), (TARGET, DST_PORT))
+
+# recieves a probe, will listen until timeout
+def recieveProbe():
+    recv_sock.settimeout(1)
+    try:
+        msgFromServer = recv_sock.recv(65565)
+        recieved = Ether(msgFromServer)
+        recieved.show()
+    except Exception as e:
+        print("No reply, timeout")
 
 def setParams():
     parser = argparse.ArgumentParser()
@@ -43,15 +66,10 @@ if __name__ == '__main__':
     recv_sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP)) 
     recv_sock.bind(('ens3', 0))
 
-    ip = IP(dst=TARGET)
-    tcp = TCP(sport=recv_sock.getsockname()[1], dport=DST_PORT, flags="S")
-    packet = ip/tcp
-    packet.show()
+    #sendProbe(1, 1)
+    #sendProbe(1, 2)
+    sendProbe(1, 3)
+    recieveProbe()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    s.sendto(bytes(packet), (TARGET, DST_PORT))
+    #print(time.time_ns())
     
-    msgFromServer = recv_sock.recv(65565)
-    recieved = Ether(msgFromServer)
-    recieved.show()
