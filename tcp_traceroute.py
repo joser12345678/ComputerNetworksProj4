c23d07d0cc7462834a6e4a5315cbb75dcdcbd446
joser12345678 <joser17@vt.edu>
2022-11-08 14:31:51 +0000

    recieves synack packets, I am stupid

--- tcp_traceroute.py
+++ tcp_traceroute.py
@@ -1,7 +1,7 @@
 import argparse
 import socket
 from sys import flags
-from scapy.all import IP, TCP
+from scapy.all import IP, TCP, Ether
 
 MAX_HOPS = 30
 DST_PORT = 80
@@ -38,17 +38,20 @@ if __name__ == '__main__':
     setParams()
 
     TARGET = socket.gethostbyname(TARGET)
+    
+    ETH_P_IP = 0x0800
+    recv_sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP)) 
+    recv_sock.bind(('ens3', 0))
+
     ip = IP(dst=TARGET)
-    tcp = TCP(sport=26121, dport=DST_PORT, flags="S")
+    tcp = TCP(sport=recv_sock.getsockname()[1], dport=DST_PORT, flags="S")
     packet = ip/tcp
     packet.show()
-    ETH_P_IP = 0x800
-    recv_sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP)) 
-    #recv_sock.bind((HOST, 26121))
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     s.sendto(bytes(packet), (TARGET, DST_PORT))
     
     msgFromServer = recv_sock.recv(65565)
-    recieved = IP(msgFromServer)
+    recieved = Ether(msgFromServer)
     recieved.show()
