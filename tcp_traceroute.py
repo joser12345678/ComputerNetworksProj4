0ff8dd6e88d5c6b08aa9ddfcac8e1db1dae60b2b
joser12345678 <joser17@vt.edu>
2022-11-11 02:22:03 +0000

    sends and recieves icmp, now we need to set it up to listen for tcp replies

--- tcp_traceroute.py
+++ tcp_traceroute.py
@@ -1,13 +1,13 @@
 import argparse
 import socket
 from sys import flags
-from scapy.all import IP, TCP, Ether, ICMP, IPerror
+from scapy.all import IP, TCP, Ether, ICMP, IPerror, raw
 import time
 
 MAX_HOPS = 30
 DST_PORT = 80
 TARGET = ""
-
+time_to_send = 0
 
 
 # sends a single probe with specified ttl
@@ -15,9 +15,8 @@ def sendProbe(my_ttl, my_id):
     ip = IP(dst=TARGET, ttl=my_ttl, id = my_id)
     tcp = TCP(sport=recv_sock.getsockname()[1], dport=DST_PORT, flags="S")
     packet = ip/tcp
-    #print(IP(raw(packet))[IP].id)
+    #packet.show()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     s.sendto(bytes(packet), (TARGET, DST_PORT))
 
@@ -25,7 +24,7 @@ def sendProbe(my_ttl, my_id):
 def recieveProbe():
     recv_sock.settimeout(1)
     try:
-        msgFromServer = recv_sock.recv(65565)
+        msgFromServer = recv_sock.recv(4096)
         recieved = Ether(msgFromServer)
         return recieved
     except Exception as e:
@@ -35,22 +34,39 @@ def recieveProbe():
 # key value pairs. The key = (ttl, id), and the value = time in ns we sent it
 def sendTraces():
     sent_packets = {}
-
-    for i in range(MAX_HOPS):
+    id = 1234
+    
+    for i in range(1, MAX_HOPS + 1):
         for j in range(3):
-            sent_packets[(i, j)] = time.time_ns()
-            sendProbe(i, j)
-
+            sent_packets[id] = [time.time_ns(), i]
+            sendProbe(i, id)
+            id = id + 1
+    
     return sent_packets
 
 # given a dictionary of probes, determine if a reply
 # is a reply to a probe
 def isProbe(probe_dict, recieved_packet):
-    packet_tuple = (recieved_packet[IP][ICMP].ttl, recieved_packet[IP][ICMP].id)
-    if packet_tuple in probe_dict:
-        print(packet_tuple)
-        return True
-    else:
+    # if ip packet is an icmp packet, parse it as such
+    # looking for icmp error payload with one of our sent probes in it
+    if recieved_packet[IP].proto == 1:
+        icmp= recieved_packet[ICMP]
+        if icmp.type != 11 or icmp.code != 0:
+            return False
+        #return True
+        icmp_error = icmp[IPerror]
+        #print(icmp_error.id)
+        if icmp_error.id in probe_dict:
+            probe_dict[icmp_error.id][0] = (time.time_ns() - probe_dict[icmp_error.id][0])/1000000
+            print(probe_dict[icmp_error.id][0])
+            return True
+        else:
+            return False
+    
+    # if the ip packet is a tcp packet, parse it as such,
+    # looking for a syn ack reply to our initial syn packet
+    elif recieved_packet[IP].proto == 6:
+        #recieved_packet.show()
         return False
 
 # drives the traceroute program, sends all probes and listens for them
@@ -58,6 +74,7 @@ def traceroute_driver():
     sent_probes = sendTraces()
     total_probes = len(sent_probes)
     timeout_count = 0
+    #print(sent_probes)
 
     while total_probes != 0 and timeout_count != 10:
         returnedProbe = recieveProbe()
@@ -65,9 +82,11 @@ def traceroute_driver():
         # if there was no returned probe, it is a timeout
         if returnedProbe == 0:
             timeout_count = timeout_count + 1
+            print('timeout')
         # if the probe is in the list, it is a probe reply
         elif isProbe(sent_probes, returnedProbe):
             total_probes = total_probes - 1
+            #print(total_probes)
     
 
 
@@ -101,20 +120,25 @@ if __name__ == '__main__':
     setParams()
 
     TARGET = socket.gethostbyname(TARGET)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     
-    ETH_P_IP = 0x0800
+    ETH_P_IP = 0x800
+    #print(ETH_P_IP)
     recv_sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP)) 
     recv_sock.bind(('ens3', 0))
 
     #sendProbe(1, 1)
-    sendProbe(1, 2)
-    #traceroute_driver()
-    recieved_packet = recieveProbe()
-    #recieved_packet.show()
-    icmp_error = recieved_packet[ICMP]
-    #icmp_error.show()
-    icmp = icmp_error[IPerror]
-    icmp.show()
+    #sendProbe(1, 2)
+    traceroute_driver()
+    # recieved_packet = recieveProbe()
+    # print(isProbe(None, recieved_packet))
+    # print(recieved_packet[IP].proto)
+    # icmp_error = recieved_packet[ICMP]
+    # print(icmp_error.type)
+    # print(icmp_error.code)
+    # icmp = icmp_error[IPerror]
+    # icmp.show()
     
     #packet_tuple = (recieved_packet[IP][ICMP][IP].ttl, recieved_packet[IP][ICMP][IP].id)
     #print(packet_tuple)
